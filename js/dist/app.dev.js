269d7bead6854159c232e4a28b9e9f12df83da68
Egor <79405864+Freekson@users.noreply.github.com>
2021-10-17 22:42:16 +0200

    Update app.dev.js

--- js/dist/app.dev.js
+++ js/dist/app.dev.js
@@ -1,25 +1,55 @@
 "use strict";
 
 $(function () {
-  var header = $('header');
-  var height = $('#header').innerHeight(); // Переменная с высотой header
+  var header = $("#header"),
+      introH = $("#intro").innerHeight(),
+      scrollOffset = $(window).scrollTop(); // Fixed Header
 
-  var scrollOffset = $(window).scrollTop(); //Сохраняем текущий скрол страницы
-
-  checkScroll(scrollOffset); //вызов функция
-
+  checkScroll(scrollOffset);
   $(window).on("scroll", function () {
     scrollOffset = $(this).scrollTop();
     checkScroll(scrollOffset);
   });
 
   function checkScroll(scrollOffset) {
-    //функция
-    if (scrollOffset >= height) {
-      //проверка высоты скрола
+    if (scrollOffset >= introH) {
       header.addClass("fixed");
     } else {
       header.removeClass("fixed");
     }
-  }
+  } // Smooth scroll
+
+
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+    var $this = $(this),
+        blockId = $this.data("scroll"),
+        blockOffset = $(blockId).offset().top;
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
+    $("html, body").animate({
+      scrollTop: blockOffset
+    }, 500);
+  }); // Menu nav toggle
+
+  $("#nav_toggle").on("click", function (event) {
+    event.preventDefault();
+    $(this).toggleClass("active");
+    $("#nav").toggleClass("active");
+  }); // Collapse
+
+  $("[data-collapse]").on("click", function (event) {
+    event.preventDefault();
+    var $this = $(this),
+        blockId = $this.data("collapse");
+    $(blockId).slideToggle();
+    $this.toggleClass("active");
+  }); // Slider
+
+  $("[data-slider]").slick({
+    infinite: true,
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
 });
